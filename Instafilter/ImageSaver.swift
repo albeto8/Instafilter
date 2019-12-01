ed083f2613db96e31d1225bc5073acd7af317a09
Mario Alberto Barragán Espinosa <>
2019-12-01 16:21:41 -0600

    First commit

---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Mario Alberto Barragán Espinosa on 01/12/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
